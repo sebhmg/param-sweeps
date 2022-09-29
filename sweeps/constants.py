1ba2a2ad13b2d7df521ca1152cfe09e7557debf7
benjamink <benjamink@mirageoscience.com>
2022-09-29 13:52:23 -0700

    Add license statement at the beginning of all package files

--- sweeps/constants.py
+++ sweeps/constants.py
@@ -1,3 +1,10 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of param-sweeps.
+#
+#  param-sweeps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
 defaults = {
     "title": "Parameter sweep",
     "run_command": "sweeps.driver",
--- sweeps/params.py
+++ sweeps/params.py
@@ -1,3 +1,10 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of param-sweeps.
+#
+#  param-sweeps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
 from copy import deepcopy
 import numpy as np
 from geoh5py.ui_json import InputFile
