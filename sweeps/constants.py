6029a65eb42ebc1c1ec55bc5c0d24f76951964b9
benjamink <benjamink@mirageoscience.com>
2022-10-06 10:05:13 -0700

    update the .constants

--- sweeps/constants.py
+++ sweeps/constants.py
@@ -5,18 +5,9 @@
 #  param-sweeps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-defaults = {
-    "title": "Parameter sweep",
-    "run_command": "sweeps.driver",
-    "conda_environment": "sweeps",
-    "monitoring_directory": None,
-    "workspace_geoh5": None,
-    "geoh5": None,
-}
-
 default_ui_json = {
     "title": "Parameter sweep",
-    "run_command": "SweepDriver.run",
+    "run_command": "sweeps.driver",
     "conda_environment": "sweeps",
     "monitoring_directory": None,
     "workspace_geoh5": None,
