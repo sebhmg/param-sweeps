c2a04abfc0d52c281139aab01bf75bed11924ad1
benjamink <benjamink@mirageoscience.com>
2022-10-11 10:53:14 -0700

    Stick to MIT lisence

--- devtools/publish.py
+++ devtools/publish.py
@@ -2,18 +2,8 @@
 #
 #  This file is part of param-sweeps.
 #
-#  param-sweeps is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  param-sweeps is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with param-sweeps.  If not, see <https://www.gnu.org/licenses/>.
+#  param-sweeps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
 
 from pathlib import Path
 
