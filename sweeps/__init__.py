b5c2405051464e5ae53d72493dead8c9836eec36
Andrew Gray <andrewg@mirageoscience.com>
2022-10-13 09:11:08 -0700

    GEOPY-649 remove unneeded import

--- sweeps/__init__.py
+++ sweeps/__init__.py
@@ -5,5 +5,4 @@
 #  param-sweeps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-from __future__ import annotations
 __version__ = "0.1.0"
